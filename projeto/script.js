cd87e047c6cb0aaf1e6766766c236be152281905
Ana Beatriz Morita <anabemorita@gmail.com>
2023-05-14 10:07:20 -0300

    Impedir que a página se recarregue no formulário

--- projeto/script.js
+++ projeto/script.js
@@ -1,7 +1,9 @@
 const formulario = document.querySelector('form');
 const inputTarefa = document.querySelector('input');
 
-function cadastrarTarefa() {
+function cadastrarTarefa(evento) {
+  // impedir que o formulário recarregue a página
+  evento.preventDefault();
   let valorInput = inputTarefa.value;
   let itemDaLista = document.createElement('li');
   itemDaLista.innerHTML = valorInput;
