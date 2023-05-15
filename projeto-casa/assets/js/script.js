d4f6e82830eaa2dc5f254ab7fee81e7fd0b0afc2
Mia Azevedo <mialuzazevedo@gmail.com>
2023-05-15 04:30:51 -0400

    adcionada funcionalidade de pomodoro e novo layout

--- projeto-casa/assets/js/script.js
+++ projeto-casa/assets/js/script.js
@@ -1,69 +1,94 @@
-const formulario = document.querySelector('form')
-const inputBox = document.getElementById('input-tarefas')
-const btnMarcarTodas = document.getElementById('btn-marcar')
-const btnApagarTodos = document.getElementById('btn-limpar')
-let lista = document.querySelector('ul') 
+const formulario = document.querySelector("form");
+const inputBox = document.getElementById("input-tarefas");
+const btnMarcarTodos = document.getElementById("btn-marcar");
+const btnApagarTodos = document.getElementById("btn-limpar");
+let lista = document.querySelector("ul");
 
-function adcionarTarefa(evento){
-    evento.preventDefault()
-    let inputTxt = inputBox.value
+function adcionarTarefa(evento) {
+  evento.preventDefault();
+  let inputTxt = inputBox.value;
 
-    let div = document.createElement('div') //cria uma div para abrigar cada tarefa
-    div.classList.add('div-tarefa') //class pra o check e o texto ficarem lado a lado
+  if (inputTxt.trim() == "") {
+    alert("Digite alguma tarefa");
+  } else {
+    let div = document.createElement("div"); //cria uma div para abrigar cada tarefa
+    div.classList.add("div-tarefa"); //class pra o check e o texto ficarem lado a lado
 
-    let check = document.createElement('input') //cria um input checkbox
-    check.setAttribute('type','checkbox')
-    check.addEventListener('change', marcarTarefa)
+    let check = document.createElement("input"); //cria um input checkbox
+    check.setAttribute("type", "checkbox");
+    check.addEventListener("change", marcarTarefa);
+    check.addEventListener("change", mudarTextoBotaoMarcarTodos);
 
-    let itemLista = document.createElement('li') //cria um li e poe o texto do input dentro
-    itemLista.innerHTML = inputTxt
+    let itemLista = document.createElement("li"); //cria um li e poe o texto do input dentro
+    itemLista.innerHTML = inputTxt;
 
-    let btnApagar = document.createElement('button')
-    btnApagar.addEventListener('click', apagarTarefa)
+    let btnApagar = document.createElement("button");
+    btnApagar.addEventListener("click", apagarTarefa);
+    let hr = document.createElement('hr')
 
-    itemLista.appendChild(check)
-    div.appendChild(itemLista) //coloca o li e check como filhos da div
-    div.appendChild(btnApagar)
-    lista.appendChild(div) // coloca a div como filha do ul
+    itemLista.appendChild(check);
+    div.appendChild(itemLista); //coloca o li e check como filhos da div
+    div.appendChild(btnApagar);
+    lista.appendChild(div); // coloca a div como filha do ul
+    lista.appendChild(hr)
+  }
+  inputBox.value = "";
 }
 
-formulario.addEventListener('submit', adcionarTarefa)
+formulario.addEventListener("submit", adcionarTarefa);
 
-function marcarTarefa(){
-    let itemLista = this.parentNode
+function marcarTarefa() {
+  let itemLista = this.parentNode;
 
-    if(this.checked){
-        itemLista.classList.add('checked')
-    }else{
-        itemLista.classList.remove('checked')
+  if (this.checked) {
+    itemLista.classList.add("checked");
+  } else {
+    itemLista.classList.remove("checked");
+  }
+}
+
+function verificarSeTodasEstaoMarcadas() {
+  let listAllCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+  let todosMarcados = true;
+  listAllCheckboxes.forEach((checkDaVez) => {
+    if (checkDaVez.checked === false) {
+      todosMarcados = false;
     }
+  });
+  return todosMarcados;
 }
 
+function mudarTextoBotaoMarcarTodos() {
+  let todosMarcados = verificarSeTodasEstaoMarcadas();
 
-function marcarTodasAsTarefas(){
-    let AllCheckboxes = document.querySelectorAll('input[type="checkbox"]')
-    let allChecked = true;
-    AllCheckboxes.forEach((checkDaVez) => {
-        if(checkDaVez.checked === false){
-            allChecked = false;
-        }
-    });
-    AllCheckboxes.forEach((checkDaVez) => {
-        checkDaVez.checked = !allChecked;
-        marcarTarefa.call(checkDaVez)
-    });
+  if (todosMarcados === true) {
+    btnMarcarTodos.innerHTML = "desmarcar todos";
+  } else {
+    btnMarcarTodos.innerHTML = "marcar todos";
+  }
 }
 
-btnMarcarTodas.addEventListener('click', marcarTodasAsTarefas) 
+function marcarTodasAsTarefas() {
+  let listAllCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+  let todosMarcados = verificarSeTodasEstaoMarcadas();
 
-function apagarTarefa(evento){
-    let item = evento.target.parentElement 
-    lista.removeChild(item)
+  listAllCheckboxes.forEach((checkDaVez) => {
+    checkDaVez.checked = !todosMarcados;
+    marcarTarefa.call(checkDaVez);
+  });
 }
 
-function apagarTodasAsTarefas(){
-    let allDivs = document.querySelectorAll('ul div')
-    allDivs.forEach((divDaVez) => lista.removeChild(divDaVez))
+btnMarcarTodos.addEventListener("click", marcarTodasAsTarefas);
+btnMarcarTodos.addEventListener("click", mudarTextoBotaoMarcarTodos);
+
+function apagarTarefa(evento) {
+  let item = evento.target.parentElement;
+  lista.removeChild(item);
 }
 
-btnApagarTodos.addEventListener('click', apagarTodasAsTarefas)
+function apagarTodasAsTarefas() {
+  let allDivs = document.querySelectorAll("ul div");
+  allDivs.forEach((divDaVez) => lista.removeChild(divDaVez));
+}
+
+btnApagarTodos.addEventListener("click", apagarTodasAsTarefas);
