1f17c60e081e39053149b1f525cb5fb3efbdb2bb
amanddmouraz <amanlsmoura@gmail.com>
2023-05-17 22:56:03 -0300

    alterações para o projeto da semanda 08

---
+++ projeto-casa-amanda/script.js
@@ -0,0 +1,121 @@
+const formulario = document.querySelector("form");
+const inputTarefa = document.querySelector("input");
+let botaoMarcarTodos = document.getElementById("marcar-todos")
+let botaoDeletarTodos = document.getElementById('limpar');
+let deletarLista = document.querySelector("ul")
+
+
+
+function marcarTodasTarefas() {
+    let itemDaListaDeTarefas = document.querySelectorAll('li');
+    itemDaListaDeTarefas.forEach(function (li) {
+        if (li.classList.contains('checked')) {
+            li.classList.remove('checked');
+        } else {
+            li.classList.add('checked');
+        }
+    });
+
+    // Verificar se todos os itens estão marcados para 
+    let todosMarcados = [...itemDaListaDeTarefas].every(function containsCheckedClass(li) {
+        return li.classList.contains('checked');
+    });
+
+    // Para atualizar o texto do botão descamarcar e marcar
+    if (todosMarcados) {
+        botaoMarcarTodos.textContent = 'Desmarcar Todos';
+    } else {
+        botaoMarcarTodos.textContent = 'Marcar Todos';
+    }
+}
+
+// Adicionar o evento de clique ao botão para chamar a função
+botaoMarcarTodos.addEventListener('click', marcarTodasTarefas);
+
+
+
+//funcao para marcar todas as tarefas
+// function marcarTodasTarefas() {
+//     let itensDaListaDeTarefas = document.querySelectorAll('li');
+//     itensDaListaDeTarefas.forEach(function (li) {
+//         if (li.classList.contains('checked')) {
+//              li.classList.remove('checked');   
+//         } else {
+//             li.classList.add('checked');
+//         }
+//     })
+// }
+
+botaoMarcarTodos.addEventListener('click', marcarTodasTarefas)
+//função para indicar que a tarefa foi concluida
+function tarefaConcluida(evento) {
+    console.log(evento.target)
+    let tarefaClicada = evento.target
+    tarefaClicada.classList.toggle('feito')
+
+}
+
+
+
+
+//inserir para cadastrar a tarefa
+// function cadastrarTarefas(evento) {
+//     //impedir que o forms recarregue a pagina
+//     evento.preventDefault();
+
+//     let itemTarefa = inputTarefa.value;
+//     let itemLista = document.createElement("li");
+//     itemLista.innerHTML = itemTarefa;
+
+//     itemLista.addEventListener("click", tarefaConcluida)
+//     let lista = document.querySelector("ul");
+
+//     lista.appendChild(itemLista);
+//     inputTarefa.value = '';
+
+
+//     let imgDelete = document.createElement('button');
+//     imgDelete.addEventListener('click', deletarTarefa);
+//     itemLista.appendChild(imgDelete)
+
+// }
+
+function cadastrarTarefas(evento) {
+    evento.preventDefault();
+    let divButton = document.createElement("div")
+
+    let itemTarefa = inputTarefa.value;
+    let itemLista = document.createElement("li");
+    itemLista.innerHTML = itemTarefa;
+
+    itemLista.addEventListener("click", tarefaConcluida);
+    let lista = document.querySelector("ul");
+
+    inputTarefa.value = '';
+
+    
+    let imgDelete = document.createElement('button');
+    imgDelete.addEventListener('click', deletarTarefa);
+
+    //
+    divButton.appendChild(itemLista);
+    divButton.appendChild(imgDelete);
+    lista.appendChild(divButton);
+}
+
+
+formulario.addEventListener("submit", cadastrarTarefas);
+
+function deletarTarefa(evento) {
+    deletarLista.removeChild(evento.target.parentElement)
+
+}
+
+
+function limparTodos() {
+    let lista = document.querySelector('ul');
+    lista.innerHTML = '';
+}
+botaoDeletarTodos.addEventListener('click', limparTodos);
+
+
