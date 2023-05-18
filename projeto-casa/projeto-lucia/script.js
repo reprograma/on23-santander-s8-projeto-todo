40ed3a31d15de0303170ea07ba43b3c04af7374d
Lucia Sindeaux <luciasindeaux@gmail.com>
2023-05-18 20:09:29 -0300

    entrega da semana 8 projeto

--- projeto-casa/projeto-lucia/script.js
+++ projeto-casa/projeto-lucia/script.js
@@ -1,6 +1,7 @@
-const formulario = document.querySelector('form');
+let formulario = document.querySelector('form');
 const inputTarefa = document.querySelector('input');
-let botaoMarcarTodos = document.getElementById('marcar-todos')
+let botaoMarcarTodos = document.getElementById('marcar-todos');
+let lista = document.querySelector('ul');
 
 
 
@@ -13,52 +14,60 @@ function marcarTarefaComoFeita(evento) {
 
 
 function marcarTodasTarefas() {
-    let itensDaListaDeTarefas = document.querySelectorAll('li');
-    itensDaListaDeTarefas.forEach(function (li) {
-        if (li.classList.contains('checked')) {
-            li.classList.remove('checked');
-        } else {
-            li.classList.add('checked');
-        }
-    });
-
-    // Verificar se todos os itens estão marcados para 
-    let todosMarcados = [...itensDaListaDeTarefas].every(function containsCheckedClass(li) {
-        return li.classList.contains('checked');
-      });
-
-    // Atualizar o texto do botão com base no estado atual
-    if (todosMarcados) {
-        botaoMarcarTodos.textContent = 'Desmarcar Todos';
+  let itensDaListaDeTarefas = document.querySelectorAll('li');
+  itensDaListaDeTarefas.forEach(function (li) {
+    if (li.classList.contains('checked')) {
+      li.classList.remove('checked');
     } else {
-        botaoMarcarTodos.textContent = 'Marcar Todos';
+      li.classList.add('checked');
     }
+  });
+
+  // Verificar se todos os itens estão marcados para 
+  let todosMarcados = [...itensDaListaDeTarefas].every(function containsCheckedClass(li) {
+    return li.classList.contains('checked');
+  });
+
+  // Atualizar o texto do botão com base no estado atual
+  if (todosMarcados) {
+    botaoMarcarTodos.textContent = 'Desmarcar Todos';
+  } else {
+    botaoMarcarTodos.textContent = 'Marcar Todos';
+  }
+
+
 }
 
 // Adicionar o evento de clique ao botão
 botaoMarcarTodos.addEventListener('click', marcarTodasTarefas);
-  
-  function marcarTarefa(evento) {
-    evento.target.classList.toggle('checked');
-  }
-  
-  function deletarTarefa(evento) {
-    ul.removeChild(evento.target.parentElement);
-  }
+
+function marcarTarefa(evento) {
+  evento.target.classList.toggle('checked');
+}
+
+function deletarTarefa(evento) {
+  lista.removeChild(evento.target.parentElement);
+}
 
 function cadastrarTarefa(evento) {
-  // impedir que o formulário recarregue a página
   evento.preventDefault();
   let valorInput = inputTarefa.value;
   let itemDaLista = document.createElement('li');
   itemDaLista.innerHTML = valorInput;
   itemDaLista.addEventListener('click', marcarTarefaComoFeita)
 
-  let lista = document.querySelector('ul');
+  let imgDelete = document.createElement('img');
+  imgDelete.src = 'img/trash.png';
+  imgDelete.classList.add('delete-img')
+  imgDelete.addEventListener('click', deletarTarefa);
+  itemDaLista.appendChild(imgDelete)
+
   lista.appendChild(itemDaLista);
   inputTarefa.value = '';
 
+
 }
+
 formulario.addEventListener('submit', cadastrarTarefa);
 
 
@@ -66,9 +75,9 @@ let botaoLimparTodos = document.getElementById('limpar');
 botaoLimparTodos.addEventListener('click', limparTodos);
 
 function limparTodos() {
-    let lista = document.querySelector('ul');
-    lista.innerHTML = '';
-  }
+  let lista = document.querySelector('ul');
+  lista.innerHTML = '';
+}
 
 formulario.appendChild(botaoLimparTodos);
 
