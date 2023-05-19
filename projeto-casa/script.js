f1b4a5bb6d67a2bf7aba0c24c28f70fb9a0b96f5
Flaviana <flaviferraz@yahoo.com.br>
2023-05-19 15:40:23 -0300

    inserção botão delete item e funcionalidades

--- projeto-casa/script.js
+++ projeto-casa/script.js
@@ -2,7 +2,9 @@ const formulario = document.querySelector('form');
 const inputTarefa = document.querySelector('input');
 const marcarTodas = document.querySelector('#marcar-todos');
 let lista = document.querySelector('ul');
+let itemLista = document.querySelector('li');
 const limpar = document.querySelector('#limpar');
+const lixeira = document.querySelector('img');
 
 //MARCAR TAREFA COMO REALIZADA
 function marcarTarefaRealizada(evento) {
@@ -20,24 +22,36 @@ function marcarTodasTarefas( ){
 
 marcarTodas.addEventListener('click', marcarTodasTarefas); 
 
+
 //LIMPAR TODAS AS TAREFAS SIMULTANEAMENTE - BOTÃO LIMPAR
-function limparTarefas (){
+function limparTarefas( ){
   lista.innerHTML = '';
 }
 
 limpar.addEventListener('click', limparTarefas);
 
+function deletarItem(evento){
+  lista.removeChild(evento.target.parentElement);
+}
+
 //CADASTRA TAREFAS
 function cadastrarTarefa(evento) {
   // impede que o formulário recarregue a página
   evento.preventDefault();
+  
   let valorInput = inputTarefa.value;
   let itemDaLista = document.createElement('li');
   itemDaLista.innerHTML = valorInput;
-  itemDaLista.addEventListener('click', marcarTarefaRealizada);
-
+    
   lista.appendChild(itemDaLista);
 
+  let trashButton = document.createElement('img');
+  trashButton.innerHTML = '<img src="/projeto-casa/trash.png">';
+   
+  lista.appendChild(trashButton);
+
+  itemDaLista.addEventListener('click', marcarTarefaRealizada);
+  
   // apaga valor do input depois de digitar
   inputTarefa.value = '';
 }
@@ -45,10 +59,6 @@ function cadastrarTarefa(evento) {
 // evento de submit, acontece quando clicar em um botao tipo submit (no nosso caso só tem um botao então ele é implicitamente do tipo submit) dentro do formulário ou quando der enter dentro de um input
 formulario.addEventListener('submit', cadastrarTarefa);
 
-function insertTrash( ){
-    let lixeira = document.createElement('img src="/projeto-casa/trash.png"');
-    lixo.appendChild(lixeira);
-}
 
 
 
