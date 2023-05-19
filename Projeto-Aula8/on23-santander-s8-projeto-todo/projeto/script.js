f9a7c4aa7cd0294c54f86571c1dd212bf01008c6
Maryanne Lima <126959124+Maryanne-limsi@users.noreply.github.com>
2023-05-19 01:17:03 -0300

    Add files via upload

---
+++ Projeto-Aula8/on23-santander-s8-projeto-todo/projeto/script.js
@@ -0,0 +1,8 @@
+const Formulario = document.querySelector (`form`);
+const inputTarefa = document.querySelector (input);
+
+function cadastrarTarefa () {
+
+    let valorInput = inputTarefa.value;
+}
+formulario.addEventlistener {'submit', cadastrarTarefa}
