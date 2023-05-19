26fe8705d8b1e7c715a556e2977a36ddc2dde4c1
Anna Caroline Miranda <annacarolinemm@gmail.com>
2023-05-18 22:51:55 -0300

    Botao_de_limpar_e_botao_lixeira

--- projeto-casa/Projeto/script.js
+++ projeto-casa/Projeto/script.js
@@ -13,9 +13,18 @@ function marcarTodas(evento) {
     for(const marcarItens of marcarTodasTarefas){
         marcarItens.classList.toggle('feito');
     }
-}
-
+} 
 marcarTodos.addEventListener('click', marcarTodas)
+
+function deletarTodos(evento) {
+    let deletarTodasTarefas = document.querySelectorAll('li');
+    for(const deletaritens of deletarTodasTarefas){
+        deletaritens.remove()
+    }
+    
+}
+limpar.addEventListener('click', deletarTodos)
+
 
 function cadastrarTarefa(evento) {
     evento.preventDefault();
@@ -23,6 +32,12 @@ function cadastrarTarefa(evento) {
     let itemDaLista = document.createElement('li');
     itemDaLista.innerHTML = valorInput;
     itemDaLista.addEventListener('click', tarefaFeita)
+
+    let buttonDeletar = document.createElement('button');
+    itemDaLista.appendChild(buttonDeletar)
+    buttonDeletar.addEventListener('click', function(){
+    itemDaLista.remove()
+        })
 
     let lista = document.querySelector('ul');
     lista.appendChild(itemDaLista);
@@ -32,5 +47,22 @@ function cadastrarTarefa(evento) {
 //só criamos o intem li da lista porq a lista já foi criada no html
 
 formulario.addEventListener('submit', cadastrarTarefa)
+
+// tentativas de Adicionar a lixeira 
+//  let buttonDeletar = document.createElement('button');
+//  buttonDeletar.innerHTML = '<img src="./trash.png" alt="Delete">';
+//  buttonDeletar.addEventListener('click', function(){
+//      itemDaLista.remove();
+//  });
+
+// let buttonDeletar = document.createElement('button');
+// let imgDeletar = document.createElement('img');
+// imgDeletar.src ='./trash.png';
+// imgDeletar.alt = 'Delete'
+// buttonDeletar.appendChild(imgDeletar);
+// buttonDeletar.addEventListener('click', function(){
+//     itemDaLista.remove()
+// })
+
 
 
