805651293c3e833d62d39a43dc0c26d07e68a176
Annie Santana <annie.santtana@gmail.com>
2023-05-19 13:52:44 -0300

    inclusão de funcionalidades

--- projeto/script.js
+++ projeto/script.js
@@ -1,11 +1,21 @@
 const formulario = document.querySelector('form');
 const inputTarefa = document.querySelector('input');
+const btMarcarTodos = document.getElementById('marcarTodos');
+let ul = document.querySelector('ul');
 
 function marcarTarefaComoFeita(evento) {
-  let tarefaClicada = evento.target;
-  tarefaClicada.classList.toggle('feito');
+  evento.preventDefault()
+  //let tarefaClicada = evento.target;
+  //tarefaClicada.classList.toggle('feito');
   // poderia ser direto assim também
   evento.target.classList.toggle('feito');
+}
+
+function marcarTodasTarefas(e) {
+  e.preventDefault()
+  ul.classList.toggle('feito')
+
+
 }
 
 function cadastrarTarefa(evento) {
@@ -16,12 +26,21 @@ function cadastrarTarefa(evento) {
   itemDaLista.innerHTML = valorInput;
   itemDaLista.addEventListener('click', marcarTarefaComoFeita)
 
-  let lista = document.querySelector('ul');
-  lista.appendChild(itemDaLista);
+  ul.appendChild(itemDaLista);
 
   // apagar valor do input depois de digitar
   inputTarefa.value = '';
+
 }
+
+
 
 // evento de submit, acontece quando clicar em um botao tipo submit (no nosso caso só tem um botao então ele é implicitamente do tipo submit) dentro do formulário ou quando der enter dentro de um input
 formulario.addEventListener('submit', cadastrarTarefa);
+
+btMarcarTodos.addEventListener('click', marcarTodasTarefas);
+
+
+//btMarcarTodos.addEventListener('click', marcarTarefaComoFeita)
+
+
